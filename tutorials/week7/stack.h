18ab0f423afd7f0be653170a7f75152a3e8ccb88
Nicholas <nicholasmulianto@gmail.com>
2019-07-16 19:59:40 +1000

    [NM]: week7 answers

--- tutorials/week7/stack.h
+++ tutorials/week7/stack.h
@@ -12,4 +12,16 @@ class Stack {
   std::vector<T> stack_;
 };
 
+template <typename T>
+void Stack<T>::push(T t) {
+  stack_.push_back(t);
+}
+
+template <typename T>
+T Stack<T>::pop() {
+  T ret_val = stack_.back();
+  stack_.pop_back();
+  return ret_val;
+}
+
 #endif  // LECTURES_WEEK7_STACK_H_
--- tutorials/week7/template_instantiation.cpp
+++ tutorials/week7/template_instantiation.cpp
@@ -7,10 +7,10 @@ T max(T a, T b) {
 
 int main() {
   int result = 7;
-  std::cout << max(1, 2) << "\n";
-  std::cout << max(1.1, 2.2) << "\n";
-  std::cout << max(1.0, 2.0) << "\n";
-  std::cout << max('a', 'z') << "\n";
-  std::cout << max(7, result) << "\n";
-  std::cout << max("cat", "dog") << "\n";
+  std::cout << max(1, 2) << "\n"; // max<int>
+  std::cout << max(1.1, 2.2) << "\n"; // max<double>
+  std::cout << max(1.0, 2.0) << "\n"; // max<double>
+  std::cout << max('a', 'z') << "\n"; // max<char>
+  std::cout << max(7, result) << "\n"; // max<int>
+  std::cout << max("cat", "dog") << "\n"; // max<const char*>
 }
